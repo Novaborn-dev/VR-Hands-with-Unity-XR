a5b0a6caf684c94ea1fdd09fdc5811fe901c0a2b
Novaborn-dev <novabornstudios@gmail.com>
2020-11-04 17:16:20 -0600

    initial commit

---
+++ Assets/XRHandController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public enum HandType
+{
+    Left,
+    Right
+}
+
+public class XRHandController : MonoBehaviour
+{
+    public HandType handType;
+    public float thumbMoveSpeed = 0.1f;
+
+    private Animator animator;
+    private InputDevice inputDevice;
+
+    private float indexValue;
+    private float thumbValue;
+    private float threeFingersValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        inputDevice = GetInputDevice();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    InputDevice GetInputDevice()
+    {
+        InputDeviceCharacteristics controllerCharacteristic = InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Controller;
+
+        if (handType == HandType.Left)
+        {
+            controllerCharacteristic = controllerCharacteristic | InputDeviceCharacteristics.Left;
+        }
+        else
+        {
+            controllerCharacteristic = controllerCharacteristic | InputDeviceCharacteristics.Right;
+        }
+
+        List<InputDevice> inputDevices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristic, inputDevices);
+
+        return inputDevices[0];
+    }
+
+    void AnimateHand()
+    {
+        inputDevice.TryGetFeatureValue(CommonUsages.trigger, out indexValue);
+        inputDevice.TryGetFeatureValue(CommonUsages.grip, out threeFingersValue);
+
+        inputDevice.TryGetFeatureValue(CommonUsages.primaryTouch, out bool primaryTouched);
+        inputDevice.TryGetFeatureValue(CommonUsages.secondaryTouch, out bool secondaryTouched);
+
+        if (primaryTouched || secondaryTouched)
+        {
+            thumbValue += thumbMoveSpeed;
+        }
+        else
+        {
+            thumbValue -= thumbMoveSpeed;
+        }
+
+        thumbValue = Mathf.Clamp(thumbValue, 0, 1);
+
+        animator.SetFloat("Index", indexValue);
+        animator.SetFloat("ThreeFingers", threeFingersValue);
+        animator.SetFloat("Thumb", thumbValue);
+    }
+}
